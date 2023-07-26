a3d4a40a20af1abe1f89081f0953c4213a8fccdd
David <uylin40@gmail.com>
2023-07-27 00:12:58 +0800

    add new style

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\CostController;
 use App\Http\Controllers\EventController;
 use App\Http\Controllers\HourController;
+use App\Http\Controllers\LogController;
+use App\Http\Controllers\WhiteListController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,3 +25,5 @@ Route::get('/', function () {
 Route::resource('costs', CostController::class);
 Route::resource('events', EventController::class);
 Route::resource('hours', HourController::class);
+Route::resource('white-lists', WhiteListController::class);
+Route::resource('logs', LogController::class);
