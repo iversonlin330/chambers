41e3bd38d7b67560a8764c050b1b08a08fd31d14
David <david.lin@zerologix.omc>
2023-08-30 10:04:05 +0800

    add new view

--- resources/views/todo.blade.php
+++ resources/views/todo.blade.php
@@ -6,33 +6,62 @@
     <div class="container-xxl flex-grow-1 container-p-y">
         <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">待辦事項</span></h4>
 
-        <!-- Basic Bootstrap Table -->
-        <div class="card">
-            {{--            <h5 class="card-header">Table Basic</h5>--}}
-            <div class="table-responsive text-nowrap">
-                <table class="table">
-                    <thead>
-                    <tr>
-                        <th>帳號</th>
-                        <th>頁面</th>
-                        <th>行為</th>
-                        <th>時間</th>
-                    </tr>
-                    </thead>
-                    <tbody class="table-border-bottom-0">
-                    @for($i = 1;$i<=5;$i++)
-                        <tr>
-                            <td>abc@com</td>
-                            <td>時數登陸</td>
-                            <td>insert</td>
-                            <td>2023/01/01 13:30:00</td>
-                        </tr>
-                    @endfor
-                    </tbody>
-                </table>
+        @for($i=0;$i<=5;$i++)
+            <div class="divider text-start-center">
+                <div class="divider-text">案件Ａ</div>
+            </div>
+            @for($j=0;$j<=2;$j++)
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card mb-4">
+                            <h5 class="card-header">
+                                <div class="form-check">
+                                    <input class="form-check-input todo-check-btn" type="checkbox" value="" id="defaultCheck3">
+                                    <label class="form-check-label" for="defaultCheck3">xxx安排諮詢時間</label>
+                                    <button class="btn-sm btn-primary me-1 collapsed" type="button"
+                                            data-bs-toggle="collapse" data-bs-target="#collapseExample{{ $i . '-' . $j }}"
+                                            aria-expanded="false" aria-controls="collapseExample">展開
+                                    </button>
+                                </div>
+{{--                                <input class="form-check-inline" type="checkbox" name="">xxx安排諮詢時間--}}
+{{--                                <button class="btn-sm btn-primary me-1 collapsed" type="button"--}}
+{{--                                        data-bs-toggle="collapse" data-bs-target="#collapseExample{{ $i . '-' . $j }}"--}}
+{{--                                        aria-expanded="false" aria-controls="collapseExample">展開--}}
+{{--                                </button>--}}
+                            </h5>
+                            <div class="card-body">
+                                <div class="collapse" id="collapseExample{{ $i . '-' . $j }}" style="">
+                                    <p>到期時間：</p>
+                                    <p>裝態：助理Ａ．助理Ｂ確認完成</p>
+                                    <p>連結：http://google.com</p>
+                                    <p>描述：開庭資料應包含照片．錄影黨</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endfor
+        @endfor
+    </div>
+    <!-- Modal -->
+    <div class="modal fade" id="modalCenter" tabindex="-1" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="modalCenterTitle">完成此項目？</h5>
+                    <button
+                        type="button"
+                        class="btn-close"
+                        data-bs-dismiss="modal"
+                        aria-label="Close"
+                    ></button>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">取消</button>
+                    <button type="button" class="btn btn-primary" id="quick_select_btn">確定</button>
+                </div>
             </div>
         </div>
-        <!--/ Basic Bootstrap Table -->
     </div>
 @endsection
 @section('script')
@@ -45,5 +74,9 @@
                 $('#main-content').toggleClass('col-md-9 col-md-12');
             });
         });
+
+        $(".todo-check-btn").click(function () {
+            $('#modalCenter').modal('show');
+        })
     </script>
 @endsection
